8a857599b237583769a55e2bc2c714d476c26a59
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-12-01 18:56:11 +0000

    d1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = await File.ReadAllLinesAsync(args[0]);
+
+var count = 0;
+List<int> left = [], right = [];
+
+foreach (var line in lines)
+{
+    var nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    left.Add(nums[0]);
+    right.Add(nums[1]);
+    count++;
+}
+
+left.Sort();
+right.Sort();
+
+var totalDist = (from i in Enumerable.Range(0, count)
+                 let l = left[i]
+                 let r = right[i]
+                 select Math.Abs(l - r)).Sum();
+
+Console.WriteLine("Total distance: {0}", totalDist);
+
+var rightCounts = right.GroupBy(o => o)
+    .ToDictionary(o => o.Key, o => o.Count());
+
+var totalSimilarity = (from n in left
+                       where rightCounts.ContainsKey(n)
+                       let c = rightCounts[n]
+                       select n * c).Sum();
+
+Console.WriteLine("Total similarity: {0}", totalSimilarity);
