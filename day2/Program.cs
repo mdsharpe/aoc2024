cef0bcd5d082b8bbf7a0c98f071c9f97d2b52e4a
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-12-02 19:49:11 +0000

    d2p2

--- day2/Program.cs
+++ day2/Program.cs
@@ -5,8 +5,9 @@ var reports = lines
     .ToArray();
 
 var safeCount = 0;
+var safeCountWithDampener = 0;
 
-foreach (var report in reports)
+bool getIsSafe(int[] report)
 {
     var deltas = Enumerable.Range(0, report.Length - 1)
         .Select(i => report[i + 1] - report[i])
@@ -15,10 +16,37 @@ foreach (var report in reports)
     var isUnsafe = deltas.Any(d => d == 0 || Math.Abs(d) > 3)
         || (deltas.Any(d => d < 0) && deltas.Any(d => d > 0));
 
-    if (!isUnsafe)
+    return !isUnsafe;
+}
+
+foreach (var report in reports)
+{
+    var isSafe = getIsSafe(report);
+
+    if (isSafe)
     {
         safeCount++;
+        safeCountWithDampener++;
+    }
+    else
+    {
+        for (var i = 0; i < report.Length; i++)
+        {
+            var dampenedReport = Enumerable.Concat(
+                report.Take(i),
+                report.Skip(i + 1).Take(report.Length - (i + 1)))
+                .ToArray();
+
+            isSafe = getIsSafe(dampenedReport);
+
+            if (isSafe)
+            {
+                safeCountWithDampener++;
+                break;
+            }
+        }
     }
 }
 
 Console.WriteLine("{0} reports are safe.", safeCount);
+Console.WriteLine("{0} reports are safe with Problem Dampener.", safeCountWithDampener);
