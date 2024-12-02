39d2e262eb54bdc7ef1b0fd9bbf833a88fdcc886
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-12-02 19:16:51 +0000

    d2p1

---
+++ day2/Program.cs
@@ -0,0 +1,24 @@
+﻿var lines = await File.ReadAllLinesAsync(args[0]);
+
+var reports = lines
+    .Select(line => line.Split(' ').Select(int.Parse).ToArray())
+    .ToArray();
+
+var safeCount = 0;
+
+foreach (var report in reports)
+{
+    var deltas = Enumerable.Range(0, report.Length - 1)
+        .Select(i => report[i + 1] - report[i])
+        .ToArray();
+
+    var isUnsafe = deltas.Any(d => d == 0 || Math.Abs(d) > 3)
+        || (deltas.Any(d => d < 0) && deltas.Any(d => d > 0));
+
+    if (!isUnsafe)
+    {
+        safeCount++;
+    }
+}
+
+Console.WriteLine("{0} reports are safe.", safeCount);
