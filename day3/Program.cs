af2c033c258ee50f2448c4f065280ded9dbe7600
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-12-03 19:37:11 +0000

    d3p2

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,20 +1,40 @@
 ﻿using System.Text.RegularExpressions;
 
-var regexMul = new Regex(@"mul\((\d+),(\d+)\)");
+var regexMul = new Regex(@"(?>mul\((\d+),(\d+)\)|do\(\)|don't\(\))");
 
 var memory = await File.ReadAllTextAsync(args[0]);
 
 var matches = regexMul.Matches(memory);
 
 var total = 0;
+var totalWithConditionals = 0;
+var mulEnabled = true;
 
 foreach (Match match in matches)
 {
-    Console.WriteLine(match);
+    if (match.Value == "do()")
+    {
+        mulEnabled = true;
+        continue;
+    }
+
+    if (match.Value == "don't()")
+    {
+        mulEnabled = false;
+        continue;
+    }
+
     var a = int.Parse(match.Groups[1].Value);
     var b = int.Parse(match.Groups[2].Value);
     var result = a * b;
+
     total += result;
+
+    if (mulEnabled)
+    {
+        totalWithConditionals += result;
+    }
 }
 
 Console.WriteLine("Total: {0}", total);
+Console.WriteLine("Total (with conditionals): {0}", totalWithConditionals);
