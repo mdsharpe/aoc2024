2c03c8d640e04ac6c42d3190491a98d6d3fe57e1
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-12-03 19:24:49 +0000

    d3p1

---
+++ day3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+var regexMul = new Regex(@"mul\((\d+),(\d+)\)");
+
+var memory = await File.ReadAllTextAsync(args[0]);
+
+var matches = regexMul.Matches(memory);
+
+var total = 0;
+
+foreach (Match match in matches)
+{
+    Console.WriteLine(match);
+    var a = int.Parse(match.Groups[1].Value);
+    var b = int.Parse(match.Groups[2].Value);
+    var result = a * b;
+    total += result;
+}
+
+Console.WriteLine("Total: {0}", total);
