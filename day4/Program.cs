e1dafbaba2f4aa59dc0891f158a913d5664f5745
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-12-04 19:58:17 +0000

    d4p2

--- day4/Program.cs
+++ day4/Program.cs
@@ -5,18 +5,25 @@
 var width = grid.Select(o => o.Length).Max();
 var height = grid.Length;
 
-(int dx, int dy)[] searchDirs = [
-    (1, 0),
-    (1, 1),
-    (0, 1),
-    (-1, 1),
-    (-1, 0),
-    (-1, -1),
-    (0, -1),
-    (1, -1)
+(int ox, int oy, int dx, int dy)[][] searchesSeqsNormal = [
+    [(0, 0, 1, 0)],
+    [(0, 0, 1, 1)],
+    [(0, 0, 0, 1)],
+    [(0, 0, -1, 1)],
+    [(0, 0, -1, 0)],
+    [(0, 0, -1, -1)],
+    [(0, 0, 0, -1)],
+    [(0, 0, 1, -1)]
 ];
 
-IEnumerable<(int x, int y)> FindInstancesOf(string word)
+(int ox, int oy, int dx, int dy)[][] searchSeqsX = [
+    [(-1, -1, 1, 1), (1, -1, -1, 1)],
+    [(-1, -1, 1, 1), (-1, 1, 1, -1)],
+    [(1, 1, -1, -1), (1, -1, -1, 1)],
+    [(1, 1, -1, -1), (-1, 1, 1, -1)],
+];
+
+IEnumerable<(int x, int y)> FindInstancesOf(string word, bool xMode)
 {
     var targetChars = word.ToCharArray();
 
@@ -24,31 +31,44 @@ IEnumerable<(int x, int y)> FindInstancesOf(string word)
     {
         for (var x = 0; x < width; x++)
         {
-            foreach (var dir in searchDirs)
+            foreach (var searchSeq in xMode ? searchSeqsX : searchesSeqsNormal)
             {
-                int sx = x, sy = y, si = 0;
-                var found = true;
+                var seqSatisfied = true;
 
-                do
+                foreach (var dir in searchSeq)
                 {
-                    if (sx < 0 || sx >= width
-                    || sy < 0 || sy >= height)
-                    {
-                        found = false;
-                    }
-                    else if (grid[sy][sx] != targetChars[si])
-                    {
-                        found = false;
-                    }
-                    else
-                    {
-                        si++;
-                        sx += dir.dx;
-                        sy += dir.dy;
-                    }
-                } while (found && si < targetChars.Length);
+                    int sx = x + dir.ox,
+                        sy = y + dir.oy,
+                        si = 0;
+                    var found = true;
 
-                if (found)
+                    do
+                    {
+                        if (sx < 0 || sx >= width
+                        || sy < 0 || sy >= height)
+                        {
+                            found = false;
+                        }
+                        else if (grid[sy][sx] != targetChars[si])
+                        {
+                            found = false;
+                        }
+                        else
+                        {
+                            si++;
+                            sx += dir.dx;
+                            sy += dir.dy;
+                        }
+                    } while (found && si < targetChars.Length);
+
+                    if (!found)
+                    {
+                        seqSatisfied = false;
+                        break;
+                    }
+                }
+
+                if (seqSatisfied)
                 {
                     yield return (x, y);
                 }
@@ -57,5 +77,8 @@ IEnumerable<(int x, int y)> FindInstancesOf(string word)
     }
 }
 
-var count = FindInstancesOf("XMAS").Count();
+var count = FindInstancesOf("XMAS", false).Count();
 Console.WriteLine("Found {0} instances of XMAS in wordsearch.", count);
+
+count = FindInstancesOf("MAS", true).Count();
+Console.WriteLine("Found {0} instances of X-MAS in wordsearch.", count);
