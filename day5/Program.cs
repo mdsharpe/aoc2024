101df8ebc73d2f6fcc87d45a9c92305c9c54a3d9
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-12-05 19:29:18 +0000

    d5p1

---
+++ day5/Program.cs
@@ -0,0 +1,13 @@
+﻿var lines = await File.ReadAllLinesAsync(args[0]);
+
+List<(int x, int y)> pageOrderingRules =
+    (from line in lines.TakeWhile(o => o.Length > 0)
+     let separatorIndex = line.IndexOf('|')
+     let x = int.Parse(line.Substring(0, separatorIndex))
+     let y = int.Parse(line.Substring(separatorIndex + 1, line.Length - (separatorIndex + 1)))
+     select (x, y)).ToList();
+
+List<List<int>> pagesToProduceInEachUpdate =
+    (from line in lines.Skip(pageOrderingRules.Count + 1)
+     select line.Split(',').Select(int.Parse).ToList()).ToList();
+
